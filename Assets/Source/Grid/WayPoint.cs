f346ea3aa4b409c8bc516c2d6d723b38d1d9e0e7
Benjamin DiGregorio <ben@digregor.io>
2021-11-29 20:41:11 -0800

    Mark certain tiles buildable; detect mouse clicks on them

--- Assets/Source/Grid/WayPoint.cs
+++ Assets/Source/Grid/WayPoint.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
-public class WayPoint : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class WayPoint : MonoBehaviour {
+    [SerializeField] bool isBuildable;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnMouseDown() {
+        if (isBuildable) {
+            Debug.Log($"Clicked: {transform.name}");
+        }
     }
 }
